30d6363de710af066592ec72511c536d00ba75b7
Gustavo Romero <gadjrs_@hotmail.com>
2019-01-21 14:13:47 -0300

    funciones para cambiar tamaño de las teclas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-h
+
+function CrearCalculadora(){
+
+	function decrementar(element){
+		element.target.style.width="21%";
+	}
+	function decrementar2(element){
+		element.target.style.width="28%";
+	}
+	function decrementar3(element){
+		element.target.style.width="87%";
+	}
+	function incrementar(element){
+		element.target.style.width="22%";
+	}
+	function incrementar2(element){
+		element.target.style.width="29%";
+	}
+	function incrementar3(element){
+		element.target.style.width="90%";
+	}
+
+}
+
+var calculadora=CrearCalculadora();
